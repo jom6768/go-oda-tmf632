8ee2d246f8ff63979919c52535a4883a4227af88
Methee Suriyakrai <jom6768@gmail.com>
2024-11-14 23:53:49 +0700

    list_and_add_but_no_data

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -9,12 +10,31 @@ import (
 	openapiclient "github.com/jom6768/go-oda-tmf632/api"
 )
 
+// createIndividual
+func createIndividual(c *gin.Context) {
+	configuration := openapiclient.NewConfiguration()
+	apiClient := openapiclient.NewAPIClient(configuration)
+
+	resp, _, err := apiClient.IndividualAPI.CreateIndividual(context.Background()).Execute()
+	log.Println("resp:", resp)
+	log.Println("err:", err)
+
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Failed to insert customer"})
+		return
+	}
+
+	c.JSON(http.StatusCreated, "")
+}
+
 // listIndividual
 func listIndividual(c *gin.Context) {
 	configuration := openapiclient.NewConfiguration()
 	apiClient := openapiclient.NewAPIClient(configuration)
 
 	resp, _, err := apiClient.IndividualAPI.ListIndividual(context.Background()).Execute()
+	log.Println("resp:", resp)
+	log.Println("err:", err)
 
 	if err != nil {
 		c.JSON(http.StatusNotFound, gin.H{"error": "Customer not found"})
@@ -26,6 +46,7 @@ func listIndividual(c *gin.Context) {
 
 func main() {
 	r := gin.Default()
+	r.POST("/tmf632/customer", createIndividual)
 	r.GET("/tmf632/customer", listIndividual)
 	r.Run(":8082")
 }
